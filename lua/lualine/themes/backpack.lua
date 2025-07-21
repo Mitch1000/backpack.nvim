74e476c80be4537f39093c01e408fad301cf2477
Mitch1000 <mwdrohan@gmail.com>
2025-07-21 14:09:26 -0400

    switches lualine theme background color to be ideal dark color

--- lua/lualine/themes/backpack.lua
+++ lua/lualine/themes/backpack.lua
@@ -4,17 +4,17 @@ local theme = require("backpack.colors").setup().theme
 local backpack = {}
 
 backpack.normal = {
-  a = { bg = theme.syn.deprecated, fg = theme.ui.bg_m4, gui = 'bold' },
-  b = { bg = theme.ui.bg_m4, fg = theme.syn.deprecated },
+  a = { bg = theme.syn.deprecated, fg = theme.ui.bg_m7, gui = 'bold' },
+  b = { bg = theme.ui.bg_m7, fg = theme.syn.deprecated },
   c = { bg = theme.ui.bg, fg = theme.syn.special1 },
-  x = { bg = theme.ui.bg_m4, fg = theme.syn.special2 },
-  y = { bg = theme.ui.bg_m4, fg = theme.syn.special2 },
-  z = { bg = theme.syn.deprecated, fg = theme.ui.bg_m4, gui = 'bold'},
+  x = { bg = theme.ui.bg_m7, fg = theme.syn.special2 },
+  y = { bg = theme.ui.bg_m7, fg = theme.syn.special2 },
+  z = { bg = theme.syn.deprecated, fg = theme.ui.bg_m7, gui = 'bold'},
 }
 
 backpack.insert = {
   a = { bg = theme.syn.string, fg = theme.ui.bg, gui = 'bold' },
-  b = { bg = theme.ui.bg_m4, fg = theme.syn.special2 },
+  b = { bg = theme.ui.bg_m7, fg = theme.syn.special2 },
   c = { bg = theme.ui.bg, fg = theme.syn.special1 },
 }
 
@@ -33,13 +33,13 @@ backpack.visual = {
 backpack.replace = {
   a = { bg = theme.syn.constant, fg = theme.ui.bg, gui = 'bold' },
   b = { bg = theme.ui.bg, fg = theme.syn.constant },
-  c = { bg = theme.ui.bg_m4, fg = theme.syn.special2 },
+  c = { bg = theme.ui.bg_m7, fg = theme.syn.special2 },
 }
 
 backpack.inactive = {
   a = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim, gui = 'bold' },
   b = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim, gui = "bold" },
-  c = { bg = theme.ui.bg_m4, fg = theme.syn.special2 },
+  c = { bg = theme.ui.bg_m7, fg = theme.syn.special2 },
 }
 
 if vim.g.backpack_lualine_bold then
